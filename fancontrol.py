c9910b2c976fd9239914dce3dc40e7e9b30b4832
erickmezaR <32782521+erickmezaR@users.noreply.github.com>
2020-07-28 22:09:26 -0500

    Update fancontrol.py

--- fancontrol.py
+++ fancontrol.py
@@ -11,15 +11,11 @@ def main():
     while True:
         vcgm = Vcgencmd()
         temp = vcgm.measure_temp()
-        print(temp)
         if  temp >= 45:
             fan.on()
-            print("fan.on()")
         elif temp <= 40:
             fan.off()
-            print("fan.off()")
         time.sleep(1)
 
 if __name__ == '__main__':
     main()
-    
